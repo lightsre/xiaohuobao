828cdf0ca9671a5f304f68aeeb828cd522dafcda
ligh <liguanghui@gotokeep.com>
2019-07-29 19:54:19 +0800

    add mysql sql

--- web/views.py
+++ web/views.py
@@ -27,8 +27,6 @@ def web_name_see(request):
             except:
                 db_result = 1
             name_info = Goods_name.objects.all()
-            context = {'name_info': name_info}
-            return render(request,'web/name_see.html',context)
         elif goods_select_html == "查询":
             goods_condition = {}
             if goods_name_html != "":
@@ -41,23 +39,49 @@ def web_name_see(request):
                 name_info = Goods_name.objects.all()
             else:
                 name_info = Goods_name.objects.filter(**goods_condition)
-            context = {'name_info': name_info}
-            return render(request,'web/name_see.html',context)
-
     else:
         name_info = Goods_name.objects.all()
-        context = {'name_info': name_info}
-        return render(request,'web/name_see.html',context)
+    context = {'name_info': name_info}
+    return render(request,'web/name_see.html',context)
 
-
+@csrf_exempt
 def web_in_see(request):
-    return render(request, 'web/info.html')
+    if request.method == "POST":
+        goods_price_html = request.POST.get('web_price_html','')
+        goods_number_html = request.POST.get('web_number_html','')
+        goods_starttime_html = request.POST.get('web_starttime_html','')
+        goods_endtime_html = request.POST.get('web_endtime_html','')
+        goods_nameid_html = request.POST.get('web_nameid_html','')
+        goods_add_html = request.POST.get('web_add_html','')
+        goods_select_html = request.POST.get('web_select_html','')
+        if goods_add_html == "新增":
+            try:
+                db_result = Goods_in.objects.create(name_id=goods_nameid_html, in_price=goods_price_html, in_number=goods_number_html)
+            except:
+                db_result = 1
+            in_info = Goods_in.objects.all()
+        elif goods_select_html == "查询":
+            goods_condition = []
+            if goods_price_html != "":
+                goods_condition.append('a.in_price=%s' % goods_price_html)
+            if goods_number_html != "":
+                goods_condition.append('a.in_number=%s' % goods_number_html)
+            goods_condition = ' or '.join(goods_condition)
+            if not goods_condition :
+		in_info = Goods_in.objects.raw('''SELECT a.in_id, a.in_price, a.in_number, a.in_time, b.name_name, b.name_model, b.name_firm FROM abcd_Goods_in a LEFT JOIN abcd_Goods_name b ON a.name_id_id = b.name_id ''')
+            else:
+                in_info = Goods_in.objects.raw('''SELECT a.in_id, a.in_price, a.in_number, a.in_time, b.name_name, b.name_model, b.name_firm FROM abcd_Goods_in a LEFT JOIN abcd_Goods_name b ON a.name_id_id = b.name_id WHERE ( %s ) ''' % goods_condition)
+    else:
+        in_info = Goods_in.objects.raw('''SELECT a.in_id, a.in_price, a.in_number, a.in_time, b.name_name, b.name_model, b.name_firm FROM abcd_Goods_in a LEFT JOIN abcd_Goods_name b ON a.name_id_id = b.name_id ''')
+        
+    context = {'in_info': in_info}
+    return render(request,'web/name_see.html',context)
 
-
+@csrf_exempt
 def web_out_see(request):
     return render(request, 'web/info.html')
 
-
+@csrf_exempt
 def web_profit_see(request):
     return render(request, 'web/info.html')
 
