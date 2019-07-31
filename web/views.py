3fa0137ecc29ec90a9bded96c55d5f5edb2cae8d
ligh <liguanghui@gotokeep.com>
2019-07-31 14:51:46 +0800

    change url

--- web/views.py
+++ web/views.py
@@ -21,6 +21,8 @@ def web_name_see(request):
         goods_firm_html = request.POST.get('web_firm_html','')
         goods_add_html = request.POST.get('web_add_html','')
         goods_select_html = request.POST.get('web_select_html','')
+        goods_url_html = request.path
+        print(goods_url_html)
         if goods_add_html == "新增":
             try:
                 db_result = Goods_name.objects.create(name_name=goods_name_html, name_model=goods_model_html, name_firm=goods_firm_html)
@@ -59,7 +61,7 @@ def web_in_see(request):
                 db_result = Goods_in.objects.create(name_id=goods_nameid_html, in_price=goods_price_html, in_number=goods_number_html)
             except:
                 db_result = 1
-            in_info = Goods_in.objects.all()
+            in_info = Goods_in.objects.raw('''SELECT a.in_id, a.in_price, a.in_number, a.in_time, b.name_name, b.name_model, b.name_firm FROM web_Goods_in a LEFT JOIN web_Goods_name b ON a.name_id_id = b.name_id ''')
         elif goods_select_html == "查询":
             goods_condition = []
             if goods_price_html != "":
@@ -72,18 +74,51 @@ def web_in_see(request):
                 goods_condition.append('in_time between %s AND %s' % (goods_starttime_html, goods_endtime_html))
             goods_condition = ' or '.join(goods_condition)
             if not goods_condition :
-		in_info = Goods_in.objects.raw('''SELECT a.in_id, a.in_price, a.in_number, a.in_time, b.name_name, b.name_model, b.name_firm FROM abcd_Goods_in a LEFT JOIN abcd_Goods_name b ON a.name_id_id = b.name_id ''')
+                in_info = Goods_in.objects.raw('''SELECT a.in_id, a.in_price, a.in_number, a.in_time, b.name_name, b.name_model, b.name_firm FROM web_Goods_in a LEFT JOIN web_Goods_name b ON a.name_id_id = b.name_id ''')
             else:
-                in_info = Goods_in.objects.raw('''SELECT a.in_id, a.in_price, a.in_number, a.in_time, b.name_name, b.name_model, b.name_firm FROM abcd_Goods_in a LEFT JOIN abcd_Goods_name b ON a.name_id_id = b.name_id WHERE ( %s ) ''' % goods_condition)
+                in_info = Goods_in.objects.raw('''SELECT a.in_id, a.in_price, a.in_number, a.in_time, b.name_name, b.name_model, b.name_firm FROM web_Goods_in a LEFT JOIN web_Goods_name b ON a.name_id_id = b.name_id WHERE ( %s ) ''' % goods_condition)
     else:
-        in_info = Goods_in.objects.raw('''SELECT a.in_id, a.in_price, a.in_number, a.in_time, b.name_name, b.name_model, b.name_firm FROM abcd_Goods_in a LEFT JOIN abcd_Goods_name b ON a.name_id_id = b.name_id ''')
+        in_info = Goods_in.objects.raw('''SELECT a.in_id, a.in_price, a.in_number, a.in_time, b.name_name, b.name_model, b.name_firm FROM web_Goods_in a LEFT JOIN web_Goods_name b ON a.name_id_id = b.name_id ''')
         
     context = {'in_info': in_info}
-    return render(request,'web/name_see.html',context)
+    return render(request,'web/in_see.html',context)
 
 @csrf_exempt
 def web_out_see(request):
-    return render(request, 'web/info.html')
+    if request.method == "POST":
+        goods_price_html = request.POST.get('web_price_html','')
+        goods_number_html = request.POST.get('web_number_html','')
+        goods_starttime_html = request.POST.get('web_starttime_html','')
+        goods_endtime_html = request.POST.get('web_endtime_html','')
+        goods_nameid_html = request.POST.get('web_nameid_html','')
+        goods_add_html = request.POST.get('web_add_html','')
+        goods_select_html = request.POST.get('web_select_html','')
+        if goods_add_html == "新增":
+            try:
+                db_result = Goods_out.objects.create(name_id=goods_nameid_html, out_price=goods_price_html, out_number=goods_number_html)
+            except:
+                db_result = 1
+            out_info = Goods_out.objects.raw('''SELECT a.out_id, a.out_price, a.out_number, a.out_time, b.name_name, b.name_model, b.name_firm FROM web_Goods_in a LEFT JOIN web_Goods_name b ON a.name_id = b.name_id ''')
+        elif goods_select_html == "查询":
+            goods_condition = []
+            if goods_price_html != "":
+                goods_condition.append('in_price=%s' % goods_price_html)
+            if goods_number_html != "":
+                goods_condition.append('in_number=%s' % goods_number_html)
+            if goods_nameid_html != "":
+                goods_condition.append('name_id=%s' % goods_nameid_html)
+            if goods_starttime_html != "" and goods_endtime_html != "":
+                goods_condition.append('in_time between %s AND %s' % (goods_starttime_html, goods_endtime_html))
+            goods_condition = ' or '.join(goods_condition)
+            if not goods_condition :
+                out_info = Goods_out.objects.raw('''SELECT a.out_id, a.out_price, a.out_number, a.out_time, b.name_name, b.name_model, b.name_firm FROM web_Goods_in a LEFT JOIN web_Goods_name b ON a.name_id = b.name_id ''')
+            else:
+                out_info = Goods_out.objects.raw('''SELECT a.out_id, a.out_price, a.out_number, a.out_time, b.name_name, b.name_model, b.name_firm FROM web_Goods_in a LEFT JOIN web_Goods_name b ON a.name_id = b.name_id  WHERE ( %s ) % goods_condition''')
+    else:
+        out_info = Goods_out.objects.raw('''SELECT a.out_id, a.out_price, a.out_number, a.out_time, b.name_name, b.name_model, b.name_firm FROM web_Goods_in a LEFT JOIN web_Goods_name b ON a.name_id = b.name_id ''')
+        
+    context = {'in_info': in_info}
+    return render(request,'web/out_see.html',context)
 
 @csrf_exempt
 def web_profit_see(request):
@@ -97,16 +132,20 @@ def login_view(request):
         username = request.POST['username']
         password = request.POST['password']
         user = authenticate(username=username, password=password)
+        error_msg = "Please check the user and password !!!"
         if user is not None:
             if user.is_active:
                 login(request, user)
-                response = HttpResponseRedirect('web/webinfo')
+                response = HttpResponseRedirect('web/webinfo/')
                 response.set_cookie('username',username,86400)
                 return response
             else:
-                return  HttpResponse('Please check the user and password, login again!')
+                return render(request,'web/login.html',{'error_msg':error_msg})
         else:
-            return  HttpResponse('With the registered password is not correct!')
+            return render(request,'web/login.html',{'error_msg':error_msg})
     else:
         return render_to_response('web/login.html')
 
+def logout_view(request):
+    logout(request)
+    return render_to_response('web/login.html')
